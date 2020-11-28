cb49bde947d7f55963bb11a26a7e02112223478d
John Anthony Balbin <swengr.johnbalbin@gmail.com>
2020-11-28 14:23:37 +0900

    React-Redux-Form revisited

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -8,14 +8,16 @@ import {
   Row,
 } from "reactstrap";
 import { Link } from "react-router-dom";
-import { Control, LocalForm, Errors } from "react-redux-form";
+import { Control, Form, Errors, actions } from "react-redux-form";
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
 const minLength = (len) => (val) => val && val.length >= len;
 const isNumber = (val) => !isNaN(Number(val));
 const validEmail = (val) =>
-  /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2, 4}$/i.test(val);
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+    val
+  );
 
 class Contact extends Component {
   constructor(props) {
@@ -27,6 +29,7 @@ class Contact extends Component {
   handleSubmit(values) {
     console.log("Current State is: " + JSON.stringify(values));
     alert("Current State is: " + JSON.stringify(values));
+    this.props.resetFeedbackForm();
   }
 
   render() {
@@ -95,7 +98,10 @@ class Contact extends Component {
             <h3>Send us your Feedback</h3>
           </div>
           <div className="col-12 col-md-9">
-            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+            <Form
+              model="feedback"
+              onSubmit={(values) => this.handleSubmit(values)}
+            >
               <Row className="form-group">
                 <Label htmlFor="firstName" md={2}>
                   First Name
@@ -199,8 +205,6 @@ class Contact extends Component {
                     className="form-control"
                     validators={{
                       required,
-                      minLength: minLength(3),
-                      maxLength: maxLength(15),
                       validEmail,
                     }}
                   />
@@ -261,7 +265,7 @@ class Contact extends Component {
                   </Button>
                 </Col>
               </Row>
-            </LocalForm>
+            </Form>
           </div>
         </div>
       </div>
---
+++ src/redux/forms.js
@@ -0,0 +1,9 @@
+export const InitialFeedback = {
+  firstName: "",
+  lastName: "",
+  telNum: "",
+  email: "",
+  agree: false,
+  contactType: "Tel",
+  message: "",
+};
